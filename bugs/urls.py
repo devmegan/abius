3eb0248b91d966b09ec931aa5b439a062cac07c9
mvmolloy <mvamolloy@gmail.com>
2021-04-05 18:22:18 +0000

    adding bug detail view/template

--- bugs/urls.py
+++ bugs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.bugs, name='bugs'),
+    path('bug_detail/<int:bug_id>', views.bug_detail, name="bug_detail")
 ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Bug
 
 # Views for Home App below
@@ -11,3 +11,14 @@ def bugs(request):
         'bugs': bugs,
     }
     return render(request, 'bugs/bugs.html', context)
+
+
+def bug_detail(request, bug_id):
+    """ view to inspect bug in detail """
+    bug = Bug.objects.get(pk=bug_id)
+
+    context = {
+        'bug': bug,
+    }
+    return render(request, 'bugs/bug_detail.html', context)
+
