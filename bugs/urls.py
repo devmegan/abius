d09c76c91bbd777453764e3abf2c664ca20a2e12
megan <mvamolloy@gmail.com>
2021-04-11 13:38:27 +0100

    add report bug url

--- bugs/urls.py
+++ bugs/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.bugs, name='bugs'),
-    path('bug_detail/<int:bug_id>', views.bug_detail, name="bug_detail"),
+    path('report_bug', views.report_bug, name="report_bug"),
     path('delete_bug/<int:bug_id>', views.delete_bug, name="delete_bug"),
     path('toggle_status/<int:bug_id>', views.toggle_status, name="toggle_status"),
     path('update_urgency/<int:bug_id>/<direction>/', views.update_urgency, name="update_urgency"),
