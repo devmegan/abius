0f73c09f4dc5f6c539cd042fb2a023fe1ece1350
megan <mvamolloy@gmail.com>
2021-04-11 10:43:03 +0100

    import redirect module to home views

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Views for Home App below
 
 
 def index(request):
     """ view returns index page """
-
+    
     return render(request, 'home/index.html')
